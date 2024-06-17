f6343dfe5e3781fe569d356c881a1eb44b2ea84e
Chiyohoho <whiteAlbum2.2294@gmail.com>
2024-06-17 20:09:56 +0700

    fix avatar

--- src/Utilities/Format/getAvatar.js
+++ src/Utilities/Format/getAvatar.js
@@ -3,7 +3,7 @@ export default function getAvatar(userData) {
     const defaultAvatarUrl = 'https://i.pinimg.com/474x/94/cb/68/94cb68baea50bb98cdab65b74e731c1c.jpg'
 
     const avatar = {
-        userAvatar: userData ? `${baseAvatarUrl}${userData.avatar}` : defaultAvatarUrl,
+        userAvatar: userData?.avatar ? `${baseAvatarUrl}${userData.avatar}` : defaultAvatarUrl,
         defaultAvatar: defaultAvatarUrl
     }
 
